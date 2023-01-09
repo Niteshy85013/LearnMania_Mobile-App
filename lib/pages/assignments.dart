1af9abf515dfe81b22dc8a2a091613da16620006
Niteshy85013 <niteshyadav85013@gmail.com>
2023-01-09 18:33:26 +0545

    assignment work start

--- lib/pages/assignments.dart
+++ lib/pages/assignments.dart
@@ -10,6 +10,52 @@ class assignmentscreen extends StatefulWidget {
 class _assignmentscreenState extends State<assignmentscreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new,
+              color: Colors.black, size: 30),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        automaticallyImplyLeading: true,
+        title: const Text(
+          "Assignment Section",
+          style: TextStyle(fontSize: 23, color: Colors.black),
+        ),
+        centerTitle: true,
+        toolbarHeight: 100.2,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(23.0),
+            child: SizedBox.fromSize(
+              size: const Size(70, 70),
+              child: ClipOval(
+                child: Material(
+                  color: const Color.fromARGB(255, 231, 111, 81),
+                  child: InkWell(
+                    splashColor: Colors.green,
+                    onTap: () {},
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.assignment, size: 50), // <-- Icon
+                        // <-- Text
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+        elevation: 0.00,
+        backgroundColor: const Color.fromARGB(255, 122, 197, 212),
+      ),
+    );
   }
 }
